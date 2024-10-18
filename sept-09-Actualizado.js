9b0c7587c20d718aeabff3db2d76695e71062b86
Ferry33-2021 <88565458+Ferry33-2021@users.noreply.github.com>
2024-10-17 22:34:10 -0500

    Add files via upload

---
+++ sept-09-Actualizado.js
@@ -0,0 +1,144 @@
+/*
+console.log("Hola Mundo");
+
+let numero1 = 45;
+let numero2 = 33;
+const pi = 3.1416;
+let estado = true;
+let direccion = "calle 3 # 8-26";
+let suma = numero1 + numero2;
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(direccion);
+console.log(suma);
+
+//concatenar valores 
+
+console.log('El primer valor es: '+ numero1);
+console.log('El segundo valor es: '+ numero2);
+
+
+//concatenar valore con el signo +
+console.log('La suma de '+ numero1 + ' y ' + numero2 + ' es: '+ suma);
+
+//concatenar valores con la coma
+console.log('La suma de' , numero1 , ' y ' , numero2 , 'es: ', suma);
+
+//concatenar valores con literal template
+console.log(`la suma de ${numero1} Y ${numero2} ES: ${suma}`);
+
+//Aqui comienza la clase del 10 de septiembre
+
+let nu1 = parseInt (prompt ("Ingrese el primer valor"));
+let nu2 = parseInt (prompt ("Ingrese el segundo valor"));
+
+let s = nu1 + nu2;
+
+//concatenar con literal template
+console.log(`El primer valor ingresado es: ${nu1} y el segundo valor es: ${nu2}`);
+console.log(`La suma es: ${s} `);
+
+//Generacion de numeros aleatorios
+//numero aleatorio entre 1 y 54
+
+//Forma 1
+let alea = Math.floor(Math.random() *55 ) +1;
+
+console.log(alea);
+*/
+
+/*
+//Estructura IF
+
+let edad = 20;
+
+if(edad >=18){
+    console.log("Es mayor de edad");
+}else{
+    console.log("Es menor de edad");
+}
+*/
+
+/*
+//Ejercicio 2 : Calificacion con if sencillo
+
+let nota;
+
+nota = parseFloat(prompt("Ingrese la nota final: "));
+
+if(nota < 3.0){
+    console.log("Reprobado");
+}else{
+    console.log("Aprobado");
+}
+*/
+
+/*
+//Ejercicio 3 : Calificacion con if anidado
+
+let calificacion;
+
+calificacion = parseFloat(prompt("Ingrese la calificacion: "));
+
+if(calificacion < 3.0){
+    console.log("Reprobado");
+}else if(calificacion < 4.0){
+    console.log("Aprobado");
+}else{
+    console.log("fuera de serie");
+}
+*/
+
+/*
+//Ejercicio de nota con if anidado y el operador y que se simboliza asi: && 
+
+let calificacion;
+
+calificacion = parseFloat(prompt("Ingrese la calificacion: "));
+
+if(calificacion < 3.0){
+    console.log("Reprobado");
+}else if(calificacion > 3.1 && calificacion <= 4.0){
+    console.log("Aprobado");
+}else{
+    console.log("fuera de serie");
+}
+*/
+
+/*
+//Ejercicio en clase sobre generaciones:
+
+let generacion;
+
+generacion = parseFloat(prompt("En que año naciste: "));
+
+if(generacion < 1883){
+    console.log("Generacion antes de Crsito");
+}else if(generacion > 1884 && generacion <= 1900){
+    console.log("Generacion perdida")
+}else if(generacion >1901 && generacion <=1924){
+    console.log("Generacion Grandiosa")
+}else if(generacion > 1925 && generacion <= 1945){
+    console.log("Generacion Silenciosa")
+}else if(generacion >1946 && generacion <=1964){
+    console.log("Generacion Boornera")
+}else if(generacion > 1965 && generacion <= 1980){
+    console.log("Generacion X")
+}else if(generacion >1981 && generacion <=1996){
+    console.log("Generacion Y")
+}else if(generacion > 1997 && generacion <= 2012){
+    console.log("Generacion Z")
+}else if(generacion >2013 && generacion <=2025){
+    console.log("Generacion Alpha")
+}
+*/
+
+
+
+
+
+
+
